675c5c41dec5ede1c2329a9af6c6ea49e9c641db
Tim K <timk@192.168.8.92>
2025-04-29 09:41:16 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import './App.css'
-import Arrivals from './components/Arrivals'
-import Email from './components/Email'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import SubHeader from './components/SubHeader'
-import Values from './components/Values'
-import Sale from './components/Sale'
-import Articles from './components/Articles'
-import Categories from './components/Categories'
-import FlyMenu from './components/FlyMenu'
+import Arrivals from './components/homePage1/Arrivals'
+import Email from './components/homePage1/Email'
+import Footer from './components/homePage1/Footer'
+import Header from './components/homePage1/Header'
+import SubHeader from './components/homePage1/SubHeader'
+import Values from './components/homePage1/Values'
+import Sale from './components/homePage1/Sale'
+import Articles from './components/homePage1/Articles'
+import Categories from './components/homePage1/Categories'
+import FlyMenu from './components/homePage1/FlyMenu'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <FlyMenu/>
-      <SubHeader/>
-      <Values/>
-      <Categories/>
-      <Arrivals/>
-      <Sale/>
-      <Articles/>
-      <Email/>
-      <Footer/>
+      <Router>
+        <Routes>
+          <Route path='/' element={''}/>
+          <Header/>
+          <FlyMenu/>
+          <SubHeader/>
+          <Values/>
+          <Categories/>
+          <Arrivals/>
+          <Sale/>
+          <Articles/>
+          <Email/>
+          <Footer/>
+        </Routes>
+      </Router>
     </>
   )
 }
